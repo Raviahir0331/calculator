a68ef73a8edb2f6a12c0351b4afceaad8a330d2f
Raviahir <ravidangar762@gmail.com>
2023-07-25 15:37:45 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,13 @@
+
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+function ClearScreen(){
+document.getElementById("result").value="";
+}
+function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
